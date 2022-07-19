a8c7cab3653abdfd836bb7b5511e2a4227ecb6bf
Neil Mastroianni <nmastroianni@longbranch.k12.nj.us>
2022-07-19 15:05:01 -0400

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,21 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
-
+const path = require("path")
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+const keys = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_KEYS)
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-google-spreadsheets`,
+      options: {
+        spreadsheetId: process.env.SPREADSHEET_ID,
+        credentials: keys,
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import React from "react"
+import UIkit from "uikit"
+import "uikit/dist/js/uikit.min.js"
+import Icons from "uikit/dist/js/uikit-icons.min.js"
+
+UIkit.use(Icons)
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <div className="">
+        <h1>NEW LBPS CURRICULUM SITE</h1>
+      </div>
+    </>
+  )
 }
