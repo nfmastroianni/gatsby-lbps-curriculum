b62ac606a2b8b13cf3a44d431d9c93d8afc47dc8
Neil Mastroianni <suancho@gmail.com>
2022-07-19 21:14:28 -0400

    testing head api

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,50 @@
-import React from "react"
-import UIkit from "uikit"
-import "uikit/dist/js/uikit.min.js"
-import Icons from "uikit/dist/js/uikit-icons.min.js"
+import React from 'react'
+import UIkit from 'uikit'
+import 'uikit/dist/js/uikit.min.js'
+import Icons from 'uikit/dist/js/uikit-icons.min.js'
+import { graphql } from 'gatsby'
 
 UIkit.use(Icons)
 
-export default function Home() {
+export default function Home({
+  data: {
+    site: {
+      siteMetadata: { title },
+    },
+  },
+}) {
   return (
     <>
       <div className="">
-        <h1>NEW LBPS CURRICULUM SITE</h1>
+        <h1>{title}</h1>
       </div>
     </>
   )
 }
+
+export function Head({
+  data: {
+    site: {
+      siteMetadata: { title },
+    },
+  },
+  location,
+  pageContext,
+  params,
+}) {
+  return (
+    <>
+      <title>{title}</title>
+    </>
+  )
+}
+
+export const query = graphql`
+  query HomeQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
