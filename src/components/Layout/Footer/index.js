e018806c48145b09ea0927b90cc203e1eb199e4f
Neil Mastroianni <nmastroianni@longbranch.k12.nj.us>
2022-07-21 14:55:43 -0400

    start footer

---
+++ src/components/Layout/Footer/index.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="bg-slate-900 min-h-[100px] mt-auto"></footer>
+    </>
+  )
+}
+
+export default Footer
