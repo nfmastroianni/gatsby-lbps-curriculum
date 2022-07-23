42063aa478367e1a7a6ef9d9d0413a808fcd51a7
Neil Mastroianni <suancho@gmail.com>
2022-07-23 18:32:27 -0400

    increase padding

--- src/components/SectionHeader/index.js
+++ src/components/SectionHeader/index.js
@@ -7,7 +7,7 @@ export default function SectionHeader({ bg, headerText }) {
         bg
           ? `bg-[${bg}]`
           : ` bg-gradient-to-bl from-emerald-700 via-emerald-900 to-emerald-800`
-      } py-2 md:py-1`}
+      } py-4 md:py-2`}
     >
       <h2 className="text-center text-white text-xl md:text-2xl lg:text-3xl md:py-2 lg:py-3 tracking-wide font-semibold">
         {headerText}
