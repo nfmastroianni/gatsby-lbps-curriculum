763ce7297cf5731196737e5c717da4eaa11f47f6
Neil Mastroianni <suancho@gmail.com>
2022-07-24 10:32:18 -0400

    add breadcrumb

--- src/pages/curricula/index.js
+++ src/pages/curricula/index.js
@@ -1,24 +1,36 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
+// import components
 import Layout from '../../components/Layout'
+import Breadcrumb from '../../components/Breadcrumb'
+import Section from '../../components/Section'
+import SectionHeader from '../../components/SectionHeader'
+// import utility functions
 import { getUniqueGradeSpans } from '../../../utils'
 
-const CurriculaIndex = ({ data, path }) => {
-  const {
+const CurriculaIndex = ({
+  data: {
     allGoogleCurriculaSheet: { nodes },
-  } = data
+  },
+  path,
+}) => {
+  // get the unique grade spans present in the Google Spreadsheet
   const spans = getUniqueGradeSpans(nodes)
-  console.log(path)
   return (
-    <Layout title={'Curricula'} path={path}>
+    <Layout siteTitle={'Curricula'} path={path}>
+      <Breadcrumb path={path} />
       <div>{JSON.stringify(spans)}</div>
     </Layout>
   )
 }
 // PROP-TYPES
 CurriculaIndex.propTypes = {
-  data: PropTypes.object,
+  data: PropTypes.shape({
+    allGoogleCurriculaSheet: PropTypes.shape({
+      nodes: PropTypes.array.isRequired,
+    }).isRequired,
+  }).isRequired,
 }
 // EXPORTS
 export default CurriculaIndex
--- utils/index.js
+++ utils/index.js
@@ -20,6 +20,38 @@ const getDepartmentImages = (images, departments) => {
 }
 
 /**
+ * It takes a path and returns an object with two properties, crumbs and pieces. The crumbs property is
+ * an array of strings that represent the path broken up into its individual pieces. The pieces
+ * property is an array of strings that represent the path broken up into its individual pieces, but
+ * with the first piece being the first item in the array, the second piece being the first and second
+ * items in the array, and so on
+ * @returns An object with two properties: crumbs and pieces.
+ */
+const getBreadcrumbs = path => {
+  if (path) {
+    const splitPath = path.split('/')
+    const pieces = []
+    splitPath.forEach(item => {
+      if (item !== '') {
+        pieces.push(item)
+      }
+    })
+    const crumbs = []
+    pieces.forEach((piece, i) => {
+      if (i === 0) {
+        crumbs.push(piece)
+      } else {
+        let arr = pieces.slice(0, i + 1)
+        crumbs.push(arr.join('/'))
+      }
+    })
+    return { crumbs, pieces }
+  } else {
+    return { crumbs: ['PATH NOT SPECIFIED'], pieces: ['PATH NOT SPECIFIED'] }
+  }
+}
+
+/**
  * It takes an array of objects, and returns an array of unique values from the property `gradeSpan` of
  * each object
  * @returns An array of unique gradeSpans
@@ -47,4 +79,10 @@ const toggleVisible = setState => {
   }
 }
 
-export { getDepartmentImages, getUniqueGradeSpans, handleToTop, toggleVisible }
+export {
+  getDepartmentImages,
+  getBreadcrumbs,
+  getUniqueGradeSpans,
+  handleToTop,
+  toggleVisible,
+}
