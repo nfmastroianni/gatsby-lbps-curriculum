bb8c3062ef0ee07cd2c6de891180e341303cea8f
Neil Mastroianni <suancho@gmail.com>
2022-07-24 07:18:50 -0400

    add conditional links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import Layout from '../components/Layout'
 import Hero from '../components/Hero'
@@ -12,7 +12,7 @@ import { HiCheck } from 'react-icons/hi'
 const Home = ({
   data: {
     site: {
-      siteMetadata: { title },
+      siteMetadata: { siteTitle },
     },
     allFile: { nodes },
   },
@@ -22,7 +22,7 @@ const Home = ({
 
   return (
     <>
-      <Layout title={title} path={path}>
+      <Layout siteTitle={siteTitle} path={path}>
         <Hero />
         <Section headerText="What We Teach">
           <h3 className="text-2xl md:text-3xl font-light text-center my-2 md:my-4 lg:my-6">
@@ -35,28 +35,52 @@ const Home = ({
             practices are followed when using their curriculum.
           </p>
           <dl className="divide-y">
-            {departments.map(({ description, id, gatsbyImageData, title }) => {
-              return (
-                <div
-                  key={id}
-                  className="grid grid-rows-1 grid-cols-1 md:grid-cols-3 pt-2 md:pt-6 pb-6 md:pb-8"
-                >
-                  <GatsbyImage
-                    image={getImage(gatsbyImageData)}
-                    alt=""
-                    className="md:col-span-1 self-center justify-self-center md:justify-self-end my-4 md:my-0 md:mr-4"
-                  />
-                  <div className="col-span-2">
-                    <dt className="font-semibold text-center text-2xl lg:text-3xl pb-2 md:pb-4 lg:pb-6">
-                      {title}
-                    </dt>
-                    <dd className="prose md:prose-lg lg:prose-xl mx-auto">
-                      {description}
-                    </dd>
+            {departments.map(
+              ({
+                description,
+                id,
+                gatsbyImageData,
+                title,
+                departmentUrl,
+                departmentUrlType,
+              }) => {
+                return (
+                  <div
+                    key={id}
+                    className="grid grid-rows-1 grid-cols-1 md:grid-cols-3 pt-2 md:pt-6 pb-6 md:pb-8"
+                  >
+                    <GatsbyImage
+                      image={getImage(gatsbyImageData)}
+                      alt=""
+                      className="md:col-span-1 self-center justify-self-center md:justify-self-end my-4 md:my-0 md:mr-4"
+                    />
+                    <div className="col-span-2">
+                      <dt className="font-semibold text-center text-2xl lg:text-3xl pb-2 md:pb-4 lg:pb-6">
+                        {title}
+                      </dt>
+                      <dd className="prose md:prose-lg lg:prose-xl mx-auto mb-6">
+                        {description}
+                      </dd>
+                      {departmentUrl && departmentUrlType === 'internal' ? (
+                        <Link
+                          to={'/'}
+                          className="px-4 py-2 bg-emerald-900 rounded text-white text-center my-4 block mx-auto max-w-screen-sm hover:bg-emerald-800 hover:text-slate-200"
+                        >
+                          Visit the {title} site.
+                        </Link>
+                      ) : departmentUrl && departmentUrlType === 'external' ? (
+                        <a
+                          href={departmentUrl}
+                          className="px-4 py-2 bg-emerald-900 rounded text-white text-center my-4 block mx-auto max-w-screen-sm hover:bg-emerald-800 hover:text-slate-200"
+                        >
+                          Visit the {title} site.
+                        </a>
+                      ) : null}
+                    </div>
                   </div>
-                </div>
-              )
-            })}
+                )
+              }
+            )}
           </dl>
         </Section>
         <Section headerText="How We Teach">
@@ -72,7 +96,7 @@ const Home = ({
           <dl className="divide-y max-w-screen-lg mx-auto">
             {practices.map(({ id, title, description, url, type }) => {
               return (
-                <div className="grid md:grid-cols-2">
+                <div key={id} className="grid md:grid-cols-2">
                   <dt className="text-xl md:text-2xl text-emerald-900 justify-self-center self-center mt-6">
                     <HiCheck className="w-6 h-6 text-emerald-900 inline mr-2" />
                     {title}
@@ -80,6 +104,24 @@ const Home = ({
                   <dd className="prose md:prose-lg lg:prose-xl my-4">
                     {description}
                   </dd>
+                  {url && type === 'internal' ? (
+                    <Link
+                      className="text-center md:col-span-2 mb-4 md:mb-6"
+                      to={url}
+                    >
+                      Learn More About {title}
+                    </Link>
+                  ) : (
+                    url &&
+                    type === 'external' && (
+                      <a
+                        className="text-center md:col-span-2 mb-4 md:mb-6 px-4 py-2 bg-emerald-900 rounded text-white my-4 max-w-screen-sm mx-auto hover:bg-emerald-800 hover:text-slate-200"
+                        href={url}
+                      >
+                        Learn More About {title}
+                      </a>
+                    )
+                  )}
                 </div>
               )
             })}
@@ -93,21 +135,31 @@ const Home = ({
 export function Head({
   data: {
     site: {
-      siteMetadata: { title },
+      siteMetadata: { siteTitle },
     },
   },
   location,
 }) {
   return (
     <>
-      <title>{title}</title>
+      <title>{siteTitle}</title>
     </>
   )
 }
 
 // PROP-TYPES
 Home.propTypes = {
-  title: PropTypes.string,
+  data: PropTypes.shape({
+    site: PropTypes.shape({
+      siteMetadata: PropTypes.shape({
+        siteTitle: PropTypes.string.isRequired,
+      }),
+    }).isRequired,
+    allFile: PropTypes.shape({
+      nodes: PropTypes.array.isRequired,
+    }).isRequired,
+  }).isRequired,
+  path: PropTypes.string.isRequired,
 }
 
 // EXPORTS
@@ -117,7 +169,7 @@ export const query = graphql`
   query HomeQuery {
     site {
       siteMetadata {
-        title
+        siteTitle
       }
     }
     allFile {
