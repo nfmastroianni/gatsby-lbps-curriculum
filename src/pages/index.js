9c6ccf064fa7af45e7761dee731348a23c74052e
Neil Mastroianni <suancho@gmail.com>
2022-07-24 10:32:03 -0400

    remove period

--- src/pages/index.js
+++ src/pages/index.js
@@ -73,7 +73,7 @@ const Home = ({
                           href={departmentUrl}
                           className="px-4 py-2 bg-emerald-900 rounded text-white text-center my-4 block mx-auto max-w-screen-sm hover:bg-emerald-800 hover:text-slate-200"
                         >
-                          Visit the {title} site.
+                          Visit the {title} site
                         </a>
                       ) : null}
                     </div>
