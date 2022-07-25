2daf429e7a3e4e605f84f305afac812a934ad534
Neil Mastroianni <suancho@gmail.com>
2022-07-25 06:15:40 -0400

    add responsive font size

--- src/components/Breadcrumb/index.js
+++ src/components/Breadcrumb/index.js
@@ -9,22 +9,21 @@ const Breadcrumb = ({ path }) => {
   return (
     <>
       <nav className="max-w-screen-2xl px-3 sm:px-6 lg:px-10 xl:px-12 py-3 sm:py-4 md:py-5 lg:py-6 mx-auto">
-        <ul className="flex my-4 text-xl text-emerald-900 font-semibold">
+        <ul className="flex my-4 text-xs md:text-lg lg:text-xl text-emerald-900 sm:font-semibold">
           <li>
             <Link to="/" className="hover:text-emerald-700">
               Home
             </Link>
-            <HiChevronRight className="inline mx-4 w-4 h-4 text-emerald-900" />
+            <HiChevronRight className="inline mx-1 sm:mx-4 w-4 h-4 text-emerald-900" />
           </li>
           {breadcrumbs.crumbs.map((crumb, i) => {
-            console.log(crumb)
             return (
               <li key={crumb} className="hover:text-emerald-700">
                 <Link to={`/${crumb}/`} className="capitalize">
                   {breadcrumbs.pieces[i]}
                 </Link>
                 {i < breadcrumbs.crumbs.length - 1 && (
-                  <HiChevronRight className="inline mx-4 w-4 h-4 text-emerald-900" />
+                  <HiChevronRight className="inline mx-1 sm:mx-4 w-4 h-4 text-emerald-900" />
                 )}
               </li>
             )
