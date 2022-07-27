b71f36f7268d6adf1db036457b5d37af007e35ef
Neil Mastroianni <suancho@gmail.com>
2022-07-27 15:01:26 -0400

    adding footer content

--- src/components/Layout/Footer/index.js
+++ src/components/Layout/Footer/index.js
@@ -1,9 +1,28 @@
 import * as React from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const Footer = () => {
   return (
     <>
-      <footer className="bg-slate-900 min-h-[100px] mt-auto"></footer>
+      <footer className="bg-slate-900 min-h-[100px] mt-auto flex place-items-center py-6">
+        <div className="max-w-sm mx-auto flex flex-col items-center">
+          <StaticImage
+            src="../../../images/lbps_logo.png"
+            alt=""
+            placeholder="blurred"
+            width={100}
+            height={100}
+            layout="fixed"
+          />
+          <p className="text-xs text-slate-100 text-center mt-4">
+            Long Branch Public Schools <br /> Together We Can, Juntos Nós
+            Podemos, Juntos Podemos
+          </p>
+          <address className="text-slate-400 text-xs not-italic font-thin tracking-wide mt-2">
+            540 Broadway Long Branch, NJ 07740
+          </address>
+        </div>
+      </footer>
     </>
   )
 }
