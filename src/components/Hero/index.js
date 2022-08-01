83b76c045802f7dd3608e4cb29f65c6d60e2a5c8
Neil Mastroianni <nmastroianni@longbranch.k12.nj.us>
2022-08-01 10:26:30 -0400

    add hero animations

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <>
       <div className="grid md:grid-rows-3 md:grid-cols-5 min-h-[250px] mx-auto px-3 sm:px-6 lg:px-10 xl:px-12 py-3 sm:py-4 md:py-5 lg:py-6 max-w-screen-2xl justify-center">
-        <div className="md:col-span-2 md:row-span-2 flex items-end justify-center">
+        <div className="md:col-span-2 md:row-span-2 flex items-end justify-center motion-safe:animate-fade-down motion-safe:md:animate-fade-right">
           <StaticImage
             src="../../images/curriculum_logo.png"
             alt=""
@@ -15,12 +15,12 @@ const Hero = () => {
             height={300}
           />
         </div>
-        <div className="prose prose-emerald md:prose-lg lg:prose-xl xl:prose-2xl md:col-span-3 row-span-1 text-center self-center">
+        <div className="prose prose-emerald md:prose-lg lg:prose-xl xl:prose-2xl md:col-span-3 row-span-1 text-center self-center motion-safe:animate-fade-up motion-safe:md:animate-fade-left">
           <h2 className="text-xl md:text-2xl lg:text-3xl my-3 text-emerald-900">
             What We Teach &amp; How We Teach It
           </h2>
         </div>
-        <div className="md:col-span-3 row-span-1">
+        <div className="md:col-span-3 row-span-1 motion-safe:animate-fade-up motion-safe:md:animate-fade-left">
           <p className="prose md:prose-lg lg:prose-xl xl:prose-2xl px-2 md:px-3 lg:px-4">
             We have an important mission here at Long Branch Public Schools. We
             strive to meet and exceed the standards set forth by the State of
@@ -30,13 +30,13 @@ const Hero = () => {
             instruction (<em>how we teach</em>).
           </p>
         </div>
-        <div className="md:col-span-2 row-span-1 p-2 mb-2 md:mb-0 self-end">
+        <div className="md:col-span-2 row-span-1 p-2 mb-2 md:mb-0 self-end motion-safe:animate-fade-up motion-safe:md:animate-fade-right">
           <h3 className="text-center font-semibold text-emerald-900 text-2xl">
             Nicole Esposito <br />
             Assistant Superintendent <br /> of Curriculum &amp; Instruction
           </h3>
         </div>
-        <div className="md:col-span-3 row-span-1 p-2 mb-2 md:mb-0 self-end">
+        <div className="md:col-span-3 row-span-1 p-2 mb-2 md:mb-0 self-end motion-safe:md:animate-fade-left">
           <h4 className="text-xl font-semibold text-center pb-2 border-b">
             Office Information
           </h4>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,32 @@ module.exports = {
     './src/components/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeDown: {
+          '0%': { opacity: 0, transform: 'translate(0, -30px)' },
+          '100%': { opacity: 1, transform: 'translate(0,0)' },
+        },
+        fadeUp: {
+          '0%': { opacity: 0, transform: 'translate(0, 30px)' },
+          '100%': { opacity: 1, transform: 'translate(0,0)' },
+        },
+        fadeRight: {
+          '0%': { opacity: 0, transform: 'translate(-30px, 0)' },
+          '100%': { opacity: 1, transform: 'translate(0,0)' },
+        },
+        fadeLeft: {
+          '0%': { opacity: 0, transform: 'translate(30px, 0)' },
+          '100%': { opacity: 1, transform: 'translate(0,0)' },
+        },
+      },
+      animation: {
+        'fade-down': 'fadeDown 1s ease-in-out 1',
+        'fade-up': 'fadeUp 1s ease-in-out 1',
+        'fade-right': 'fadeRight 1s ease-in-out 1',
+        'fade-left': 'fadeLeft 1s ease-in-out 1',
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 }
