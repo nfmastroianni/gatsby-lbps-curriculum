2593e9224ff6ff31e4f5e960e8f8302bb121ce16
Neil Mastroianni <nmastroianni@longbranch.k12.nj.us>
2022-08-08 14:44:19 -0400

    allow class override make responsive

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,11 +1,13 @@
 import * as React from 'react'
 import SectionHeader from '../SectionHeader'
 
-export default function Section({ children, headerText, id }) {
+export default function Section({ children, headerText, id, className }) {
   return (
-    <section id={id}>
+    <section id={id} className={` ${className}`}>
       {headerText && <SectionHeader headerText={headerText} />}
-      <div className="mx-auto py-2 px-4 max-w-screen-2xl">{children}</div>
+      <div className="mx-auto py-2 px-3 sm:px-6 md:px-8 lg:px-10 xl:px-12 max-w-screen-2xl">
+        {children}
+      </div>
     </section>
   )
 }
