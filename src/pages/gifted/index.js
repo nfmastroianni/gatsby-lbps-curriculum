6d01e6a6d5e9894587764742e2be0cdc561f676f
Neil Mastroianni <nmastroianni@longbranch.k12.nj.us>
2022-08-08 14:46:11 -0400

    add content

--- src/pages/gifted/index.js
+++ src/pages/gifted/index.js
@@ -2,7 +2,8 @@ import * as React from 'react'
 import Layout from '../../components/Layout'
 import Breadcrumb from '../../components/Breadcrumb'
 import { graphql } from 'gatsby'
-
+import Section from '../../components/Section'
+import Heading from '../../components/Heading'
 const GiftedAndTalented = ({
   data: {
     site: {
@@ -14,6 +15,19 @@ const GiftedAndTalented = ({
   return (
     <Layout siteTitle={siteTitle} path={path}>
       <Breadcrumb path={path} />
+      <Section>
+        <div className="prose prose-emerald md:prose-lg lg:prose-xl xl:prose-2xl divide-y mx-auto">
+          <Heading level={2} className="text-center">
+            Gifted &amp; Talented (Enrichment)
+          </Heading>
+          <p className="py-4">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi vel
+            labore repellendus maxime ipsum quis qui nam, officia mollitia
+            tempora, necessitatibus dolores fugit veritatis, magnam adipisci cum
+            placeat. Eos, dolore!
+          </p>
+        </div>
+      </Section>
     </Layout>
   )
 }
