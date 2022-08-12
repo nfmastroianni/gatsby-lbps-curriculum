9a0a385e0c2b1491576190e7b2160a6aa54b4363
Neil Mastroianni <suancho@gmail.com>
2022-08-12 10:56:49 -0400

    fix dark mode styles

--- src/components/Blockquote/index.js
+++ src/components/Blockquote/index.js
@@ -5,7 +5,7 @@ import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 const QuoteIcon = ({ children, className }) => {
   return (
     <span
-      className={`font-serif text-emerald-900 absolute -left-10 -top-14 text-[8rem] hidden sm:block transition duration-500 ease-in-out  motion-safe:group-hover:scale-105 origin-bottom-left motion-safe:group-hover:-rotate-3 ${className}`}
+      className={`font-serif text-emerald-900 dark:text-emerald-200 absolute -left-10 -top-14 text-[8rem] hidden sm:block transition duration-500 ease-in-out  motion-safe:group-hover:scale-105 origin-bottom-left motion-safe:group-hover:-rotate-3 ${className}`}
       aria-hidden="true"
       style={{ textShadow: '2px 2px 4px darkgray' }}
     >
@@ -24,11 +24,13 @@ const Blockquote = ({
 }) => {
   return (
     <blockquote
-      className={`max-w-screen-sm my-4 md:my-6 lg:my-8 p-4 md:p-6 shadow relative rounded border mx-auto group ${className}`}
+      className={`max-w-screen-sm my-4 md:my-6 lg:my-8 xl:my-10 p-4 md:p-6 shadow dark:shadow-emerald-600 relative rounded border dark:border-gray-500 mx-auto group ${className}`}
     >
       <QuoteIcon>&ldquo;</QuoteIcon>
-      <p className="font-serif italic">{quote}</p>
-      <cite className="text-emerald-900 text-sm not-italic flex justify-center items-center my-4 gap-4">
+      <p className="prose prose-emerald md:prose-lg lg:prose-xl xl:prose-2xl mx-auto dark:prose-invert font-serif italic">
+        {quote}
+      </p>
+      <cite className="text-emerald-900 dark:text-emerald-200 text-sm not-italic flex justify-center items-center my-4 gap-4">
         {image && (
           <GatsbyImage
             image={getImage(image)}
